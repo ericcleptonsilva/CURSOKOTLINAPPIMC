f0cb8261b2f3db56f7cfa08963c37fc068a86685
ericcleptonsilva <ericcleptonsilva@gmail.com>
2021-08-05 00:42:26 -0300

    testando o ciclo de vida

--- app/src/main/java/com/example/curso_kotlin_app_imc/MainActivity.kt
+++ app/src/main/java/com/example/curso_kotlin_app_imc/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.curso_kotlin_app_imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.w("lifecycle","Criando a tela")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("lifecycle","A Tela está visível!")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("lifecycle","A Tela está liberada para você usar!")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("lifecycle","A Tela está pausada até voce focar na tela!")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("lifecycle","A Tela está em segundo plano até voce ativar de novo!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w("lifecycle","A Tela foi destruída!")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.w("lifecycle","A Tela está retornando para você!")
     }
 }
