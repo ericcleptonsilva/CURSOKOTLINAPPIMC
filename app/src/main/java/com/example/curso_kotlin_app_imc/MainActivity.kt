5a159fb71e4d69f2fb9102412437d58f441c40da
ericcleptonsilva <ericcleptonsilva@gmail.com>
2021-08-07 15:15:40 -0300

    Finalizando as aulas com a regra de negocio

--- app/src/main/java/com/example/curso_kotlin_app_imc/MainActivity.kt
+++ app/src/main/java/com/example/curso_kotlin_app_imc/MainActivity.kt
@@ -1,43 +1,45 @@
 package com.example.curso_kotlin_app_imc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
 
 class MainActivity : AppCompatActivity() {
+
+    val titleTXT = findViewById<TextView>(R.id.titleTXT)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.w("lifecycle","Criando a tela")
+        setListeners()
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.w("lifecycle","A Tela está visível!")
+
+    private fun setListeners() {
+        val pesoEdt = findViewById<TextView>(R.id.pesoEdit)
+        val alturaEdt = findViewById<TextView>(R.id.alturaEdit)
+
+        val btCalcular = findViewById<Button>(R.id.BtCalcular)
+        pesoEdt?.doOnTextChanged{text, _, _, _->
+            titleTXT?.text = text}
+        alturaEdt?.doAfterTextChanged { text ->
+            Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show() }
+        btCalcular?.setOnClickListener { calcularIMC(pesoEdt.text.toString(), alturaEdt.text.toString()) }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.w("lifecycle","A Tela está liberada para você usar!")
+    private fun calcularIMC(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if (peso != null && altura != null ){
+            val imc = peso / (altura + altura)
+            titleTXT.text =  "Seu IMC é: \n%.2f".format(imc)
+        }
+
     }
 
-    override fun onPause() {
-        super.onPause()
-        Log.w("lifecycle","A Tela está pausada até voce focar na tela!")
-    }
 
-    override fun onStop() {
-        super.onStop()
-        Log.w("lifecycle","A Tela está em segundo plano até voce ativar de novo!")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.w("lifecycle","A Tela foi destruída!")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.w("lifecycle","A Tela está retornando para você!")
-    }
 }
